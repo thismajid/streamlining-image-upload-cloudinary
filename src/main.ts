4924cb6340de632f8524827f74033b84f784e2f7
Majid Khorshidian <mkhorshidian72@gmail.com>
2023-11-27 11:09:06 +0330

    Add custom logger with winston

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { WinstonModule } from 'nest-winston';
 import { AppModule } from './modules/app/app.module';
 import { SwaggerInit } from './configs/swagger.config';
 import { APP_PORT } from './configs/global.config';
+import { CustomLoggerService } from './common/logger/custom-logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const customLoggerService = new CustomLoggerService();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: WinstonModule.createLogger(customLoggerService.createLoggerConfig),
+  });
   SwaggerInit(app);
   await app.listen(APP_PORT);
 }
