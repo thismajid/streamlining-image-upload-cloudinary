909e6a8d7ed508c53430c84c4c461acfea63d2d0
majid khorshidian <m.khorshidian@amnmoj.local>
2023-11-28 09:18:48 +0330

    Update env example

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -1,5 +1,5 @@
 import {
-  MONGO_DATABSE,
+  MONGO_DATABASE,
   MONGO_HOST,
   MONGO_PASSWORD,
   MONGO_PORT,
@@ -10,7 +10,7 @@ import {
 export function getMongoURL() {
   const connection =
     NODE_ENV == 'development'
-      ? `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABSE}`
-      : `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PASSWORD}/${MONGO_DATABSE}`;
+      ? `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`
+      : `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PASSWORD}/${MONGO_DATABASE}`;
   return connection;
 }
